323fd63df8c8f5e866d8f4e0d5a1ea03df2c951e
joeyscodes <joeysdigitaltools+github@gmail.com>
2026-04-28 01:48:12 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("reservationForm").addEventListener("submit", function(e) {
-e.preventDefault();
-
-document.getElementById("confirmation").classList.remove("hidden");
-
-this.reset();
-});
